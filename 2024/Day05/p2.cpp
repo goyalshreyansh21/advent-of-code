2c36b1bf02b5dfe3faba1d7b8c2b153a9e47ccf9
goyalshreyansh21 <91679502+goyalshreyansh21@users.noreply.github.com>
2024-12-05 11:13:43 +0530

    day 5

---
+++ 2024/Day05/p2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    ifstream inputFile("text.txt");
+    string line;
+
+    unordered_set<string> s;
+    bool parse = true;
+
+    int ans = 0;
+
+    while(getline(inputFile, line)) {
+        if(line.empty())
+            parse = false;
+        
+        if(parse) {
+            s.insert(line);
+        } else if(!line.empty()) {
+            vector<int> numbers;
+            replace(line.begin(), line.end(), ',', ' ');
+            istringstream ss(line);
+            int n;
+            while (ss >> n) 
+                numbers.push_back(n);
+
+            bool valid = true;
+            for(int i=0; i<numbers.size() && valid; i++) 
+                for(int j=i+1; j<numbers.size() && valid; j++) 
+                    if(s.count(to_string(numbers[j]) + "|" + to_string(numbers[i])))
+                        valid = false;
+            
+            if(!valid) {
+                bool swapped = true;
+                while (swapped) {
+                    swapped = false;
+                    for (size_t i = 0; i < numbers.size(); ++i) {
+                        for (size_t j = i + 1; j < numbers.size(); ++j) {
+                            if (s.count(to_string(numbers[j]) + "|" + to_string(numbers[i]))) {
+                                swap(numbers[i], numbers[j]);
+                                swapped = true;
+                            }
+                        }
+                    }
+                }
+                ans += numbers[numbers.size()/2];
+            }
+        }
+    }
+
+    cout<<ans;
+    
+    inputFile.close();
+    return 0;
+}
